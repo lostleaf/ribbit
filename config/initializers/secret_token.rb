9c1e64e0d423c6d99072f05ea7d9ec48fe64555d
lostleaf <jack951753@gmail.com>
2014-01-02 14:40:07 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ribbit::Application.config.secret_token = '91d67d1297e86466f7eb097d64c2c57059c63f886b4b6b15d03ba632d0178144dcc9a7329cda3c1d95930a059d55609305bc8e9e29622b1c9c7598607a60c1db'
