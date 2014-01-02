8e7882e1ba0cbc98f3ab3dedbc3c9d60e6e71ada
lostleaf <jack951753@gmail.com>
2014-01-02 17:56:27 +0800

    finish users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to @user, nocice: "Thank you for signing up"
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
