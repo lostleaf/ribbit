d26ea18141fa7036134139b63792e4133d91e650
lostleaf <jack951753@gmail.com>
2014-01-05 16:05:54 +0800

    add a js char counter

--- app/controllers/ribbits_controller.rb
+++ app/controllers/ribbits_controller.rb
@@ -1,2 +1,12 @@
 class RibbitsController < ApplicationController
+  def index
+     @ribbits = Ribbit.all
+     @ribbit = Ribbit.new
+  end
+  def create
+    ribbit = Ribbit.new(params[:ribbit])
+    ribbit.user_id = current_user.id
+    flash[:error] = "Your Ribbit was over 140 characters" unless ribbit.save
+    redirect_to ribbits_url
+  end
 end
